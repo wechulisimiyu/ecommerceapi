bcd26e124ca1ead52baa792cc5d3d394e700188e
wechu07 <wechuli017@gmail.com>
2022-10-30 18:17:00 +0300

    server

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
+
+// loading the config files
+dotenv.config({ path: './config/config.env' })
+
+// connect the db
+connectDB()
+
+const app = express()
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// HTTP request logger middleware 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use("/api/users", userRoute)
+
+const port = process.env.PORT || 5000
+
+// server running in either
+// production or development mode
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${port}`)
+})
