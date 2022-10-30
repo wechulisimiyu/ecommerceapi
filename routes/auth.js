0937df31679eaee821938838866740bcb5fe1779
wechu07 <wechuli017@gmail.com>
2022-10-30 20:09:48 +0300

    register and login routes

---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
+
+// @desc: User register
+// Route: POST/
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        // encrypted password
+        password: CryptoJS.AES.encrypt(req.body.password , process.env.PASSWORD_SECRET).toString()
+    })
+
+    try {
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}) 
+
+// @desc: User login
+// Route: POST/
+router.post("/login", async (req, res) => {
+    // find user within db
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        // error if username is not found
+        !user && res.status(401).json("Wrong username")
+
+        // decrypting the password
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASSWORD_SECRET
+        )
+
+        // should never be revealed in the db
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        // error if password is incorrect
+        originalPassword !== req.body.password && res.status(401).json("Wrong password")
+
+        // we can destructure the password, and not send it
+        const { password, ...others }= user._doc
+        
+        // if successful    
+        res.status(200).json(others)
+    } catch (err) {
+        // error status send instead
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
