62291653f95285c3d15b31d47be20fc8d974946f
wechu07 <wechuli017@gmail.com>
2022-10-31 01:04:50 +0300

    finish cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,83 @@
-const router = require('express').Router()
+const Cart = require("../models/Cart")
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken")
+
+const router = require("express").Router()
+
+// @desc: create a new cart
+// User or guest
+// ROUTE POST/
+// http://localhost:5000/api/products
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body)
+
+  try {
+    const savedCart = await newCart.save()
+    res.status(200).json(savedCart)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// @desc: update cart
+// user or guest
+// ROUTE POST/
+// http://localhost:5000/api/products
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart)
+  } catch (err) {
+    res.status(500).json(err)
+  
+})
+
+// @desc: delete something on the cart
+// user or guest
+// ROUTE POST/
+// http://localhost:5000/api/products
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id)
+    res.status(200).json("Cart has been deleted...")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// @desc: cget user cart
+// Only admin can do this
+// ROUTE POST/
+// http://localhost:5000/api/
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// @desc: get all carts
+// Only admin can do this
+// ROUTE POST/
+// http://localhost:5000/api/carts
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find()
+    res.status(200).json(carts)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
