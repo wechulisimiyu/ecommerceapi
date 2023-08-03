6fff6cbcbb91452ba0e3e20bacc04eb75b13b430
wechu07 <wechu017@gmail.com>
2023-08-03 11:37:41 +0300

    update product docs

--- api/routes/product.js
+++ api/routes/product.js
@@ -12,28 +12,125 @@ const {
   getAllProducts,
 } = require("../controllers/productControllers");
 
-// @desc: create a new product
-// Only admin can do this
-// ROUTE POST/
-// http://localhost:5000/api/products
+/**
+ * @openapi
+ * /api/products:
+ *   post:
+ *     tags: [Product]
+ *     summary: Create a new product
+ *     description: Create a new product (Admin only).
+ *     security:
+ *       - bearerAuth: []
+ *       - adminAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NewProduct'
+ *     responses:
+ *       200:
+ *         description: Product created successfully
+ *       403:
+ *         description: You are not allowed to do that!
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post("/", verifyTokenAndAdmin, createNewProduct);
 
-// @desc: updating product
-// ROUTE PUT/
+/**
+ * @openapi
+ * /api/products/{id}:
+ *   put:
+ *     tags: [Product]
+ *     summary: Update a product
+ *     description: Update a product (Admin only).
+ *     security:
+ *       - bearerAuth: []
+ *       - adminAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Product ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProductUpdate'
+ *     responses:
+ *       200:
+ *         description: Product updated successfully
+ *       403:
+ *         description: You are not allowed to do that!
+ *       500:
+ *         description: Internal server error
+ */
+
 router.put("/:id", verifyTokenAndAdmin, updateProduct);
 
-// @desc: delete product
-// ROUTE DELETE/
+/**
+ * @openapi
+ * /api/products/{id}:
+ *   delete:
+ *     tags: [Product]
+ *     summary: Delete a product
+ *     description: Delete a product (Admin only).
+ *     security:
+ *       - bearerAuth: []
+ *       - adminAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Product ID
+ *     responses:
+ *       200:
+ *         description: Product deleted successfully
+ *       403:
+ *         description: You are not allowed to do that!
+ *       500:
+ *         description: Internal server error
+ */
+
 router.delete("/:id", verifyTokenAndAdmin, deleteProduct);
 
-// @desc: get product
-// any user can get any product
-// ROUTE GET/
+/**
+ * @openapi
+ * /api/products/find/{id}:
+ *   get:
+ *     tags: [Product]
+ *     summary: Get a single product
+ *     description: Get details of a single product.
+ *     responses:
+ *       200:
+ *         description: Product retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
+
 router.get("/find/:id", getSingleProduct);
 
-// @desc: get all products
-// any user can get all products
-// ROUTE GET/
+/**
+ * @openapi
+ * /api/products:
+ *   get:
+ *     tags: [Product]
+ *     summary: Get all products
+ *     description: Get all products.
+ *     responses:
+ *       200:
+ *         description: Products retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
+
 router.get("/", getAllProducts);
 
 module.exports = router;
