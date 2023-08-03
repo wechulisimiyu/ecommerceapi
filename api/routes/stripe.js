4584bd24a255d906a6c7812a430dba39f161cf90
wechu07 <wechu017@gmail.com>
2023-08-03 11:47:44 +0300

    update payment route docs

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,6 +1,35 @@
 const router = require("express").Router();
 const stripeCharge = require("../controllers/stripeController");
 
+/**
+ * @openapi
+ * /api/payment:
+ *   post:
+ *     tags: [Payment]
+ *     summary: Make a payment using Stripe
+ *     description: Make a payment using Stripe with the provided token ID and amount.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               tokenId:
+ *                 type: string
+ *               amount:
+ *                 type: integer
+ *                 description: Amount in cents (USD)
+ *             required:
+ *               - tokenId
+ *               - amount
+ *     responses:
+ *       200:
+ *         description: Payment successful
+ *       500:
+ *         description: Payment error
+ */
+
 router.post("/payment", stripeCharge);
 
 module.exports = router;
